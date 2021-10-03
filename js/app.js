f2c4eace8b9e29171e5347643cd0c755f37c30d1
YCrhis <htcrhis@gmail.com>
2021-10-02 19:47:53 -0500

    fixing styles

--- js/app.js
+++ js/app.js
@@ -5,10 +5,3 @@ gsap.fromTo(
     { y: "-30vh" },
     { y: 0, duration: 1.5, delay: 1 }
 );
-gsap.fromTo(".social__media", { y: 200 }, { y: 0, delay: 2 });
-
-window.addEventListener("scroll", function () {
-    let social = document.querySelector(".social__media");
-    social.classList.toggle("socialMedia__scroll", window.scrollY > 300);
-    console.log("funco");
-});
